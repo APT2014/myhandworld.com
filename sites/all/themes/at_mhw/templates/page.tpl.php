9c25415efc3e2e850e340b7e8e802c96f6e8a08a
apcomvananh <phuong.nguyenanh06@gmail.com>
2014-04-25 20:56:33 +0700

    cmd

--- sites/all/themes/at_mhw/templates/page.tpl.php
+++ sites/all/themes/at_mhw/templates/page.tpl.php
@@ -48,13 +48,28 @@
 						<?php print render($page['content_top']); ?>
 					<?php endif; ?>
 					<div id="main" class="clearfix">
-						<div id="content" class="column grid-12 alpha omega  " role="main">
-							<div class="section">
-								<?php if ($page['content']): ?>
-									<?php print render($page['content']); ?>
-								<?php endif; ?>
+						<?php if($page['sidebar_second']): ?>
+							<div id="content" class="column grid-8 alpha" role="main">
+								<div class="section">
+									<?php if ($page['content']): ?>
+										<?php print render($page['content']); ?>
+									<?php endif; ?>
+								</div>
+							</div>	
+							<aside id="sidebar-second" class="column grid-4 omega" role="complementary">
+								<div class="section">
+									<?php print render($page['sidebar_second']); ?>
+								</div>
+							</aside>
+						<?php else: ?>
+							<div id="content" class="column grid-12 alpha omega" role="main">
+								<div class="section">
+									<?php if ($page['content']): ?>
+										<?php print render($page['content']); ?>
+									<?php endif; ?>
+								</div>
 							</div>
-						</div>
+						<?php endif; ?>
 					</div>
 				</div>
 			</div>
